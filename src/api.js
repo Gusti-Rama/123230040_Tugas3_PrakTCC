7e3160fde8be2c91025ff8794ce04b8405116fcd
Gusti-Rama <gustirama309@gmail.com>
2026-05-13 20:19:23 +0700

    change api url

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:3000/api/v1/notes";
+const API_URL = "https://backend-rama-73763759634.us-central1.run.app/";
 
 export const getNotes = () => axios.get(API_URL);
 export const createNote = (data) => axios.post(API_URL, data);
