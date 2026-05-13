a7e976eb1e66acb45d780858162f08dd0737413a
Gusti-Rama <gustirama309@gmail.com>
2026-05-13 20:24:25 +0700

    fix api url

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "https://backend-rama-73763759634.us-central1.run.app/";
+const API_URL = "https://backend-rama-73763759634.us-central1.run.app/api/v1/notes";
 
 export const getNotes = () => axios.get(API_URL);
 export const createNote = (data) => axios.post(API_URL, data);
